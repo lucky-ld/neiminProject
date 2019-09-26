af6c47c75ab2b8e8ee6689d4cb731c9fdfbe3a7f
lucky-ly <1666329311@qq.com>
2019-09-26 18:51:30 +0800

    Spring-boot 三个小空的工程

--- site/src/main/java/com/zpark/site/SiteApplication.java
+++ site/src/main/java/com/zpark/site/SiteApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SiteApplication {
 
+        public void aaa () {
+            System.out.println("aaaaaaaaaa");
+        }
     public static void main(String[] args) {
+
         SpringApplication.run(SiteApplication.class, args);
     }
 
